2743a57214bfee788aac6af099bd4190c7a61d4d
Mike Smith <mike.smith1@earthlink.net>
2018-04-11 14:51:25 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.Eig2ut67WSARnckMZADxlzCiR,
+  consumer_secret: process.env.BgbDdszfSQybMMkSJYh3tzBX6EQbapl2UGFf7vvoOA8vuPfWbC,
+  access_token_key: process.env.983436774507769856-WeyPCFx3oHlvhnzIyQHbbXnuE4alCXK,
+  access_token_secret: process.env.Cuf8cYaf3U0lzAg5SSWTycvO5YfuG2tDOtJQA8cjCLpIH
+};
+
+exports.spotify = {
+  id: process.env.89f4f82b9afb4a78ab4f93db0d87d586,
+  secret: process.env.d5b64227aafa496db5506cfcc3b0eda5
+};
