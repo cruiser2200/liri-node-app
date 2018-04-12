02cde3ca102aa6bc8cc7da1595b768d25390773c
Mike Smith <mike.smith1@earthlink.net>
2018-04-12 14:34:27 -0500

    my-tweets command is now working

--- keys.js
+++ keys.js
@@ -2,13 +2,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.Eig2ut67WSARnckMZADxlzCiR,
-  consumer_secret: process.env.BgbDdszfSQybMMkSJYh3tzBX6EQbapl2UGFf7vvoOA8vuPfWbC,
-  access_token_key: process.env.983436774507769856-WeyPCFx3oHlvhnzIyQHbbXnuE4alCXK,
-  access_token_secret: process.env.Cuf8cYaf3U0lzAg5SSWTycvO5YfuG2tDOtJQA8cjCLpIH
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.89f4f82b9afb4a78ab4f93db0d87d586,
-  secret: process.env.d5b64227aafa496db5506cfcc3b0eda5
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
+
